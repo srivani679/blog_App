8e485ecb4084d004890553ff737fe076abaf34e5
srivani679 <vanib444666@gmail.com>
2021-06-07 18:01:42 +0530

    initial commit

---
+++ models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+# Create your models here.
+class Post(models.Model):
+    STATUS_CHOICES=(('draft','Draft'),('published','Published'))
+    title=models.CharField(max_length=256)
+    slug=models.SlugField(max_length=264,unique_for_date='publish')
+    author=models.ForeignKey(User,related_name='blogApp_posts',on_delete=models.DO_NOTHING)
+    body=models.TextField()
+    publish=models.DateTimeField(default=timezone.now)
+    created=models.DateTimeField(auto_now_add=True)
+    publish=models.DateTimeField(auto_now=True)
+    status=models.CharField(max_length=10,choices=STATUS_CHOICES,default='draft')
+
+    class Meta:
+        ordering=('-publish',)
+    def __str__(self):
+        return self.title
